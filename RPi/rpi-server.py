b158b30743ce66310103cfb6a4c9193429a02a37
yaya-zhang <yaya.zhang@student.howest.be>
2024-06-07 10:45:30 +0200

    adding some comments for more readablility

--- RPi/rpi-server.py
+++ RPi/rpi-server.py
@@ -17,25 +17,30 @@ server_socket = None
 server_thread = None
 shutdown_flag = threading.Event()
 
+# set servo motor anfle
 def set_servo_angle(angle):
     duty = angle / 18 + 2
     GPIO.output(SERVO_PIN, True)
     servo.ChangeDutyCycle(duty)
-    time.sleep(1)
+
+    time.sleep(1) # servo to move
     GPIO.output(SERVO_PIN, False)
-    servo.ChangeDutyCycle(0)
+    servo.ChangeDutyCycle(0) # stop servo
     print(f"Servo moved to {angle} degrees")
 
+# setup servo socket , start listening for connections
 def setup_socket_server():
     global server_socket, server_thread, shutdown_flag
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('0.0.0.0', 8500))
-    server_socket.settimeout(0.2)
-    server_socket.listen(1)
+    server_socket.bind(('0.0.0.0', 8500)) # binding all interfaces on the port 8500
+    server_socket.settimeout(0.2) # set timeout for socket operations
+    server_socket.listen(1) # listen for incoming connections
     
+    # start a thread to accept incoming connection
     server_thread = threading.Thread(target=accept_connections, args=(shutdown_flag,), daemon=True)
     server_thread.start()
 
+# accetpt incoming client connection
 def accept_connections(shutdown_flag):
     global client_socket
     print("Accepting connections")
@@ -48,17 +53,18 @@ def accept_connections(shutdown_flag):
         except socket.timeout:
             pass
 
+# communication with client side
 def handle_client(sock, shutdown_flag):
     global cap
     try:
         while not shutdown_flag.is_set():
-            data = sock.recv(1024)
+            data = sock.recv(1024) # receive data from client
             if not data:
                 break
             message = data.decode(errors='ignore')
             print(f"Received from client: {message}")
             if message == 'start_video':
-                cap = cv2.VideoCapture(0)
+                cap = cv2.VideoCapture(0) # open external camera
                 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
                 send_video(sock)
             elif message == 'cat orange':
@@ -76,14 +82,15 @@ def handle_client(sock, shutdown_flag):
             cap.release()
         sock.close()
 
+# send video frame to client
 def send_video(sock):
     try:
         while True:
-            ret, frame = cap.read()
+            ret, frame = cap.read() # read frame from camera
             if not ret:
                 break
             _, buffer = cv2.imencode('.jpg', frame)
-            sock.sendall(buffer.tobytes() + b"END_FRAME")
+            sock.sendall(buffer.tobytes() + b"END_FRAME") # add delimiter to seprate frame
     except Exception as e:
         print(f"Error sending video frame: {e}")
 
